de54b54d337e0ff3c8e064834f9b443348ffbeca
Brisk4t <atharvapcat@gmail.com>
2026-02-21 21:56:57 -0700

    Fixed right icon

--- graphics.py
+++ graphics.py
@@ -84,7 +84,7 @@ class GraphicsEngine:
         Left: 0-3 signal sprites (wireless health)
         Right: Contact health bar
         """
-        sprite_size = 8
+        sprite_size = 13
         spacing = 2
         
         # Left side - Signal sprites (max 3)
@@ -112,7 +112,7 @@ class GraphicsEngine:
         bar_y = (DISPLAY_HEIGHT - bar_height) // 2
         
         # Draw contact icon above bar
-        self._draw_contact_icon(bar_x - 2, bar_y - 8)
+        self._draw_contact_icon(bar_x - 11, bar_y - 8)
         
         # Draw health bar outline
         for px in range(bar_width):
@@ -131,14 +131,14 @@ class GraphicsEngine:
     
     def _draw_heart_icon(self, x, y):
         """Draw wireless/signal indicator icon (8x8) from sprite data"""
-        if "heart_icon" in SPRITE_DATA and SPRITE_DATA["filled_heart_icon"]:
-            icon_bitmap = SPRITE_DATA["filled_heart_icon"][0]
+        if "heart_icon" in SPRITE_DATA and SPRITE_DATA["empty_heart_icon"]:
+            icon_bitmap = SPRITE_DATA["empty_heart_icon"][0]
             self._draw_bitmap(icon_bitmap, x, y)
     
     def _draw_contact_icon(self, x, y):
         """Draw contact/touch indicator icon (8x8) from sprite data"""
-        if "contact_icon" in SPRITE_DATA and SPRITE_DATA["contact_icon"]:
-            icon_bitmap = SPRITE_DATA["contact_icon"][0]
+        if "contact_icon" in SPRITE_DATA and SPRITE_DATA["filled_heart_icon"]:
+            icon_bitmap = SPRITE_DATA["filled_heart_icon"][0]
             self._draw_bitmap(icon_bitmap, x, y)
     
     def _draw_bitmap(self, bitmap_data, x, y):
